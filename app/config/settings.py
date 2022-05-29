e5427ec9cc94a175a6875fda2555664a194c3820
ceferinot <ceferinot@gmail.com>
2022-05-29 01:05:53 -0300

    Hardware

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'core.login',
     'core.homepage',
+    'core.hardWare',
     
 ]
 
---
+++ app/core/hardWare/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from core.hardWare.models import WorkStation
+# Register your models here.
+admin.site.register(WorkStation)
